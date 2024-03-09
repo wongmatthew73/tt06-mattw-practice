31781d04ffdf9c97c37370b3ea0cc51c4565ff74
wongmatthew73 <mattwillwong@yahoo.com>
2024-03-09 01:02:23 -0700

    2nd push

--- src/pwm.v
+++ src/pwm.v
@@ -1,24 +1,39 @@
 `default_nettype none
 `timescale 1ns/1ns
-module pwm (
+module pwm #(
+    parameter WIDTH = 8,
+    parameter INVERT = 0
+    )(
     input wire clk,
     input wire reset,
-    output wire out,
-    input wire [7:0] level
+    output reg out,
+    input wire [WIDTH-1:0] level
     );
     
-    reg [7:0] counter;
+    reg [WIDTH-1:0] count;
+    wire pwm_on = count < level;
+    //reg [7:0] counter;
     
     
     always @(posedge clk) begin
     	if(reset) begin
-    		counter <= 0;
+    		count <= 1'b0;
     	end
     	else begin
-    		counter <= counter + 1'b1;
+    		count <= count + 1'b1;
     	end
     end
     
-    assign out = counter < level;
+    always @(posedge clk) begin
+    	if(reset) begin
+    		out <= 0;
+    	end
+    	else begin
+    		out <= INVERT == 1'b0 ? pwm_on : ! pwm_on;
+    	end
+    end
+    
+    //assign out = reset ? 1'b0:
+    	
 	
 endmodule
